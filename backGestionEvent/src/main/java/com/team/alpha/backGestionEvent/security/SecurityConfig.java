b565b5b68c63393f612c15cac0cded6702fc2b30
AlHamine <oriontheroot@gmail.com>
2023-11-04 23:51:14 +0000

    Fichier de demarrage modified

--- backGestionEvent/src/main/java/com/team/alpha/backGestionEvent/security/SecurityConfig.java
+++ backGestionEvent/src/main/java/com/team/alpha/backGestionEvent/security/SecurityConfig.java
@@ -42,22 +42,22 @@ public class SecurityConfig{
 		return authenticationConfiguration.getAuthenticationManager();
 	}
 	
-	@Bean
-	SecurityFilterChain configureSecurity(HttpSecurity http) throws Exception {
-		return http
-				.csrf(csrf -> csrf.disable())
-				.cors(withDefaults())
-				.sessionManagement(management -> management
-						.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.authorizeRequests(authorizeRequests -> authorizeRequests
-						.requestMatchers(HttpMethod.POST, "/login").permitAll()
-						.anyRequest().authenticated())
-				.exceptionHandling().authenticationEntryPoint(exceptionHandler).and()
-				.addFilterBefore(authenticationFilter,
-						UsernamePasswordAuthenticationFilter.class)
-				.httpBasic(withDefaults())
-				.build();
-	}
+	// @Bean
+	// SecurityFilterChain configureSecurity(HttpSecurity http) throws Exception {
+	// 	return http
+	// 			.csrf(csrf -> csrf.disable())
+	// 			.cors(withDefaults())
+	// 			.sessionManagement(management -> management
+	// 					.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+	// 			.authorizeRequests(authorizeRequests -> authorizeRequests
+	// 					.requestMatchers(HttpMethod.POST, "/login").permitAll()
+	// 					.anyRequest().authenticated())
+	// 			.exceptionHandling().authenticationEntryPoint(exceptionHandler).and()
+	// 			.addFilterBefore(authenticationFilter,
+	// 					UsernamePasswordAuthenticationFilter.class)
+	// 			.httpBasic(withDefaults())
+	// 			.build();
+	// }
 
 
 	
