285a04a55bad5853971006165b368308f16366e9
gaye-00 <gayeabdoulaye163@gmail.com>
2023-11-11 00:32:36 +0000

    delete files

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,6 +17,82 @@ import { useNavigate } from "react-router-dom";
 import HomePage from "./HomePage.jsx";
 import Checkbox from "@mui/material/Checkbox";
 
+import { styled, useTheme } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+import Slider from "@mui/material/Slider";
+import IconButton from "@mui/material/IconButton";
+import PauseRounded from "@mui/icons-material/PauseRounded";
+import PlayArrowRounded from "@mui/icons-material/PlayArrowRounded";
+import FastForwardRounded from "@mui/icons-material/FastForwardRounded";
+import FastRewindRounded from "@mui/icons-material/FastRewindRounded";
+import VolumeUpRounded from "@mui/icons-material/VolumeUpRounded";
+import VolumeDownRounded from "@mui/icons-material/VolumeDownRounded";
+
+const WallPaper = styled("div")({
+  position: "absolute",
+  width: "100%",
+  height: "100%",
+  top: 0,
+  left: 0,
+  overflow: "hidden",
+  background: "linear-gradient(rgb(255, 38, 142) 0%, rgb(255, 105, 79) 100%)",
+  transition: "all 500ms cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s",
+  "&:before": {
+    content: '""',
+    width: "140%",
+    height: "140%",
+    position: "absolute",
+    top: "-40%",
+    right: "-50%",
+    background:
+      "radial-gradient(at center center, rgb(62, 79, 249) 0%, rgba(62, 79, 249, 0) 64%)",
+  },
+  "&:after": {
+    content: '""',
+    width: "140%",
+    height: "140%",
+    position: "absolute",
+    bottom: "-50%",
+    left: "-30%",
+    background:
+      "radial-gradient(at center center, rgb(247, 237, 225) 0%, rgba(247, 237, 225, 0) 70%)",
+    transform: "rotate(30deg)",
+  },
+});
+
+const Widget = styled("div")(({ theme }) => ({
+  padding: 16,
+  borderRadius: 16,
+  width: 343,
+  maxWidth: "100%",
+  margin: "auto",
+  position: "relative",
+  zIndex: 1,
+  backgroundColor:
+    theme.palette.mode === "dark" ? "rgba(0,0,0,0.6)" : "rgba(255,255,255,0.4)",
+  backdropFilter: "blur(40px)",
+}));
+
+const CoverImage = styled("div")({
+  width: 100,
+  height: 100,
+  objectFit: "cover",
+  overflow: "hidden",
+  flexShrink: 0,
+  borderRadius: 8,
+  backgroundColor: "rgba(0,0,0,0.08)",
+  "& > img": {
+    width: "100%",
+  },
+});
+
+const TinyText = styled(Typography)({
+  fontSize: "0.75rem",
+  opacity: 0.38,
+  fontWeight: 500,
+  letterSpacing: 0.2,
+});
+
 function Login({ setEstAuthentifie }) {
   const [user, setUser] = useState({
     username: "",
@@ -134,6 +210,21 @@ function Login({ setEstAuthentifie }) {
   //   <CreatePrestataire />;
   // };
 
+  // add
+  const theme = useTheme();
+  const duration = 200; // seconds
+  const [position, setPosition] = React.useState(32);
+  const [paused, setPaused] = React.useState(false);
+  function formatDuration(value) {
+    const minute = Math.floor(value / 60);
+    const secondLeft = value - minute * 60;
+    return `${minute}:${secondLeft < 10 ? `0${secondLeft}` : secondLeft}`;
+  }
+  const mainIconColor = theme.palette.mode === "dark" ? "#fff" : "#000";
+  const lightIconColor =
+    theme.palette.mode === "dark" ? "rgba(255,255,255,0.4)" : "rgba(0,0,0,0.4)";
+  // add
+
   if (isAuthenticated) {
     // console.log("client".toLowerCase() == sessionStorage.getItem("role"));
     if (sessionStorage.getItem("role") === "client") {
@@ -156,58 +247,55 @@ function Login({ setEstAuthentifie }) {
   } else {
     return (
       <div>
-        <Backdrop
-          open={true}
-          style={{
-            backgroundColor: "white",
+        <WallPaper />
+
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            mt: "10px", // Ajustez la marge supérieure
+            mx: "20px", // Ajustez la marge horizontale
           }}
         >
-          <Box
-            sx={{
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              transform: "translate(-50%, -50%)",
-              mt: "10px", // Ajustez la marge supérieure
-              mx: "20px", // Ajustez la marge horizontale
-            }}
+          <Stack
+            spacing={2}
+            alignItems="center"
+            boxShadow="0 0 5px blue"
+            display="flex"
+            justifyContent="center"
           >
-            <Stack
-              spacing={2}
-              alignItems="center"
-              boxShadow="0 0 5px blue"
-              display="flex"
-              justifyContent="center"
-            >
-              <TextField
-                name="username"
-                label="Username"
-                onChange={handleChange}
-                sx={{ p: "10px" }} // Ajustez la marge intérieure pour le TextField
-              />
-              <TextField
-                type="password"
-                name="password"
-                label="Password"
-                onChange={handleChange}
-                sx={{ p: "10px" }} // Ajustez la marge intérieure pour le TextField
-              />
-              <Button variant="outlined" color="primary" onClick={login}>
-                Login
-              </Button>
-              {/* <CreatePrestataire /> */}
-              <div style={{ color: "blue" }}>
-                <Checkbox {...label} onChange={handleChangeCustomer} />{" "}
-                {"Creer un compte client"}
-              </div>
-              <div style={{ color: "blue" }}>
-                <Checkbox {...label} onChange={handleChangeCustomer} />{" "}
-                {"Être prestataire"}
-              </div>
-              {/* <CreateCustumer /> */}
-            </Stack>
-          </Box>
-        </Backdrop>
+            <TextField
+              name="username"
+              label="Username"
+              onChange={handleChange}
+              sx={{ p: "10px" }} // Ajustez la marge intérieure pour le TextField
+            />
+            <TextField
+              type="password"
+              name="password"
+              label="Password"
+              onChange={handleChange}
+              sx={{ p: "10px" }} // Ajustez la marge intérieure pour le TextField
+            />
+            <Button variant="outlined" color="primary" onClick={login}>
+              Login
+            </Button>
+            {/* <CreatePrestataire /> */}
+            <div style={{ color: "blue" }}>
+              <Checkbox {...label} onChange={handleChangeCustomer} />{" "}
+              {"Creer un compte client"}
+            </div>
+            <div style={{ color: "blue" }}>
+              <Checkbox {...label} onChange={handleChangeCustomer} />{" "}
+              {"Être prestataire"}
+            </div>
+            {/* <CreateCustumer /> */}
+          </Stack>
+        </Box>
+
+        <Box sx={{ width: "100%", overflow: "hidden" }}></Box>
         <Snackbar
           open={open}
           autoHideDuration={3000}
