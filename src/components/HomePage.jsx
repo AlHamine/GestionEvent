18aff59171d63c10434cce4e4848655e9bbd7c2f
gaye-00 <gayeabdoulaye163@gmail.com>
2023-11-28 01:43:37 +0000

    alway in the front

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,13 +1,129 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Animefront from "./Animefront.json";
-import Animefront2 from "./Animefront2.json";
+// import Animefront2 from "./Animefront2.json";
 import Animation1 from "./Animation1.json";
+import Prestation from "./Prestation.json";
 import Lottie from "lottie-react";
 import TaskAltIcon from "@mui/icons-material/TaskAlt";
+import DoneAllIcon from "@mui/icons-material/DoneAll";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
+import { SERVER_URL } from "../constants.js";
+import TextField from "@mui/material/TextField";
+import DatePicker from "react-datepicker";
+import { useNavigate } from "react-router-dom";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from "@mui/material";
 
 function HomePage() {
+  const [open, setOpen] = useState(false);
+  const gmail = sessionStorage.getItem("UserMail");
+  const token = sessionStorage.getItem("jwt");
+  // const [events, setEvents] = useState([]);
+  const [event, setEvent] = useState({
+    nomEvent: "",
+    date: "",
+    desciption: "",
+    lieu: "",
+    organisateur: { idc: Number(sessionStorage.getItem("idClient")) },
+  });
+
+  useEffect(() => {
+    fetch(SERVER_URL + `client/mail?mail=${gmail}`, {
+      headers: { "Content-Type": "application/json", Authorization: token },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        sessionStorage.setItem("idClient", data.idc);
+        sessionStorage.setItem("n", data.nom);
+        sessionStorage.setItem("p", data.prenom);
+        // sessionStorage.setItem("client", data);
+        // d = data;
+      })
+      .catch((err) => console.error(err))
+      .catch((err) => console.log(err));
+  }, [gmail, token]);
+
+  // useEffect(() => {
+  //   fetchEvents();
+  // }, []);
+
+  // const fetchEvents = () => {
+  //   const token = sessionStorage.getItem("jwt");
+  //   fetch(SERVER_URL + "api/evenements", {
+  //     headers: { Authorization: token },
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => setEvents(data._embedded.evenements))
+  //     .catch((err) => console.error(err));
+  //   console.log(token);
+  // };
+  // const history = useHistory();
+  const navigate = useNavigate();
+
+  const addEvent = (event) => {
+    fetch(SERVER_URL + "event", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(event),
+    })
+      .then((response) => {
+        if (response.ok) {
+          navigate("/events");
+        } else {
+          alert("Something went wrong");
+        }
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSave = () => {
+    if (event.organisateur.idc !== 0) {
+      // props.addEvent(event);
+      addEvent(event);
+    } else {
+      event.organisateur.idc = Number(sessionStorage.getItem("idClient"));
+      setEvent(event);
+      // props.addEvent(event);
+      addEvent(event);
+      // alert("Veuillez re-essayez encore   ");
+    }
+    setEvent({
+      nomEvent: "",
+      date: "",
+      desciption: "",
+      lieu: "",
+      organisateur: { idc: Number(sessionStorage.getItem("idCient")) },
+    });
+    handleClose();
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setEvent((prevEvent) => ({
+      ...prevEvent,
+      [name]: value,
+    }));
+  };
+  const handleDateChange = (date) => {
+    setEvent((prevEvent) => ({
+      ...prevEvent,
+      date,
+    }));
+  };
+
   return (
     <div>
       <div
@@ -39,7 +155,9 @@ function HomePage() {
             </div>
 
             <Stack direction="row" spacing={2}>
-              <Button variant="outlined">CREER UN EVENEMENT</Button>
+              <Button variant="outlined" onClick={handleClickOpen}>
+                CREER UN EVENEMENT
+              </Button>
             </Stack>
           </div>
         </div>
@@ -68,7 +186,7 @@ function HomePage() {
           {/* Image à droite */}
           <img
             src="https://media.istockphoto.com/id/974238866/photo/audience-listens-to-the-lecturer-at-the-conference.jpg?s=612x612&w=0&k=20&c=p_BQCJWRQQtZYnQlOtZMzTjeB_csic8OofTCAKLwT0M="
-            alt="Description de l'image"
+            alt="img"
             style={{ width: "100%", height: "auto" }}
           />
         </div>
@@ -121,11 +239,114 @@ function HomePage() {
           {/* Image à droite */}
           <img
             src="https://img.freepik.com/free-vector/festive-calendar-event-holiday-celebration-party-work-schedule-planning-project-management-deadline-idea-office-managers-excited-colleagues_335657-1610.jpg"
-            alt="Description de l'image"
+            alt="img"
             style={{ width: "100%", height: "auto" }}
           />
         </div>
       </div>
+
+      <div
+        style={{
+          display: "flex",
+          boxShadow: "0px 5px 10px rgba(0, 0, 0, 1)",
+          backgroundColor: "#f2f1f8",
+        }}
+      >
+        <div style={{ flex: 1, backgroundColor: "#fff" }}>
+          {/* Contenu texte à gauche */}
+          <h1
+            style={{
+              color: "blue",
+              textAlign: "center",
+              margin: "0 auto",
+              marginTop: "20px",
+            }}
+          >
+            Préstation
+          </h1>
+          <div
+            style={{ lineHeight: "2", color: "#747379", marginLeft: "30px" }}
+          >
+            <div
+              style={{ marginTop: "20%", marginLeft: "30px", color: "blue" }}
+            >
+              <div>EVENT-PRO votre meilleur service de prestation</div>
+            </div>
+            <div style={{ marginBottom: "10px", color: "blue" }}>
+              <DoneAllIcon sx={{ color: "blue" }} /> {" Dans le mariage.  "}
+            </div>
+            <div style={{ marginBottom: "10px", color: "blue" }}>
+              <DoneAllIcon /> {" Seremonie religieuse.  "}
+            </div>
+            <div style={{ marginBottom: "10px", color: "blue" }}>
+              <DoneAllIcon /> {" Dans les meetings.  "}
+            </div>
+            <div style={{ marginBottom: "10px", color: "blue" }}>
+              <DoneAllIcon /> {" Dans les finerailles. "}
+            </div>
+            <div style={{ marginBottom: "10px", color: "blue" }}>
+              <DoneAllIcon /> {" Economiser son temps evec EVENT-PRO.  "}
+            </div>
+          </div>
+        </div>
+        <div style={{ flex: 1 }}>
+          {/* Image à droite */}
+          <Lottie animationData={Prestation} />
+        </div>
+      </div>
+
+      <Dialog open={open} onClose={handleClose} fullWidth maxWidth="sm">
+        <DialogTitle>Nouvel événement</DialogTitle>
+        <DialogContent>
+          <Stack spacing={2} mt={1}>
+            <TextField
+              label="Nom de l'événement"
+              name="nomEvent"
+              autoFocus
+              variant="standard"
+              value={event.nomEvent}
+              onChange={handleChange}
+            />
+            <DatePicker
+              name="dkdk"
+              value={event.date}
+              onChange={handleDateChange}
+              dateFormat="dd-MM-yyyy"
+            />
+            <TextField
+              label="Desciption"
+              name="desciption"
+              variant="standard"
+              value={event.desciption}
+              onChange={handleChange}
+            />
+            <TextField
+              label="Lieu"
+              name="lieu"
+              variant="standard"
+              value={event.lieu}
+              onChange={handleChange}
+            />
+          </Stack>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Annuler</Button>
+          <Button onClick={handleSave}>Enregistrer</Button>
+        </DialogActions>
+      </Dialog>
+      {open && (
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            background: "rgba(0, 0, 0, 0.5)",
+            zIndex: 1000,
+          }}
+        ></div>
+      )}
     </div>
   );
 }
--- src/components/ResponsiveAppBar.js
+++ src/components/ResponsiveAppBar.js
@@ -1,44 +1,54 @@
-import * as React from "react";
+import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
+// import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
+// import AdbIcon from "@mui/icons-material/Adb";
 import DataArrayIcon from "@mui/icons-material/DataArray";
 import Badge from "@mui/material/Badge";
 import MailIcon from "@mui/icons-material/Mail";
 import AirlinesIcon from "@mui/icons-material/Airlines";
 
+import Stack from "@mui/material/Stack";
+import SnackbarContent from "@mui/material/SnackbarContent";
+
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import { Link } from "react-router-dom";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from "@mui/material";
 
 import Logout from "./Logout";
 
-const pages = ["Evenements", "Prestataires", "Blog"];
+const pages = ["Evenements", "Prestataires", "Populaire"];
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  // const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
+  const [open, setOpen] = useState(false);
+  const [openNotif, setOpenNotif] = useState(false);
+  // const handleOpenNavMenu = (event) => {
+  //   setAnchorElNav(event.currentTarget);
+  // };
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
   };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+  // const handleCloseNavMenu = () => {
+  //   setAnchorElNav(null);
+  // };
 
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
@@ -48,16 +58,39 @@ function ResponsiveAppBar() {
     handleCloseUserMenu();
   };
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleClickOpenNotif = () => {
+    setOpenNotif(true);
+  };
+
+  const handleCloseNotif = () => {
+    setOpenNotif(false);
+  };
+
+  const action = (
+    <Button color="secondary" size="small">
+      Supprimer
+    </Button>
+  );
+
   return (
     <div>
       <AppBar position="static">
-        <Container maxWidth="xl">
+        <Container maxWidth="xl" sx={{ marginLeft: "0px", marginRight: "0px" }}>
           <Toolbar disableGutters>
             <Typography
               variant="h6"
               noWrap
               component="a"
               href="#app-bar-with-responsive-menu"
+              sx={{ marginLeft: "0px" }}
             >
               <Link to="/" passHref>
                 <Button
@@ -154,7 +187,13 @@ function ResponsiveAppBar() {
                 
               ))}
             </Box> */}
-            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {/* <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}></Box> */}
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: { xs: "none", md: "flex" },
+              }}
+            >
               {pages.map((page) =>
                 page === "Evenements" ? (
                   <Button>
@@ -179,7 +218,7 @@ function ResponsiveAppBar() {
                 ) : (
                   <Button
                     key={page}
-                    onClick={handleCloseNavMenu}
+                    // onClick={handleCloseNavMenu}
                     sx={{ my: 2, color: "white", display: "block" }}
                   >
                     {page}
@@ -193,7 +232,7 @@ function ResponsiveAppBar() {
               aria-label="show new mails"
               sx={{ display: { xs: "none", md: "block" } }}
             >
-              <Badge badgeContent={4} color="error">
+              <Badge badgeContent={4} color="error" onClick={handleClickOpen}>
                 <MailIcon />
               </Badge>
             </IconButton>
@@ -202,15 +241,20 @@ function ResponsiveAppBar() {
               size="large"
               aria-label="show new notifications"
               sx={{ marginRight: 3 }}
+              onClick={handleClickOpenNotif}
             >
               <Badge badgeContent={17} color="error">
                 <NotificationsIcon />
               </Badge>
             </IconButton>
 
-            <Box sx={{ flexGrow: 0 }}>
+            {/* flexGrow: 0  */}
+            <Box sx={{ marginRight: "1px" }}>
               <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <IconButton
+                  onClick={handleOpenUserMenu}
+                  sx={{ p: 0, marginRight: "1px" }}
+                >
                   <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
                 </IconButton>
               </Tooltip>
@@ -251,6 +295,96 @@ function ResponsiveAppBar() {
           </Toolbar>
         </Container>
       </AppBar>
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        fullWidth
+        maxWidth="sm"
+        // sx={{ width: "100%", position: "absolute", right: 0, height: "100%" }}
+        sx={{ position: "absolute", right: 80, top: 8 }}
+      >
+        <DialogTitle>
+          <div
+            style={{
+              position: "absolute",
+              left: "35%",
+            }}
+          >
+            Nouveaux messages
+          </div>
+        </DialogTitle>
+        <DialogContent></DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Annuler</Button>
+          {/* <Button onClick={handleSave}>Enregistrer</Button> */}
+        </DialogActions>
+      </Dialog>
+      {open && (
+        <div
+          style={{
+            // position: "left",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            background: "rgba(0, 0, 0, 0.5)",
+            zIndex: 1000,
+            marginLeft: "100%",
+          }}
+        ></div>
+      )}
+
+      <Dialog
+        open={openNotif}
+        onClose={handleCloseNotif}
+        fullWidth
+        maxWidth="sm"
+        // sx={{ width: "100%", position: "absolute", right: 0, height: "100%" }}
+        sx={{ position: "absolute", right: 80, top: 8 }}
+      >
+        <DialogTitle> Nouvelles notifications</DialogTitle>
+        <DialogContent>
+          <Stack spacing={2} sx={{ maxWidth: 600 }}>
+            <SnackbarContent message="I love snacks." action={action} />
+            <SnackbarContent
+              message={
+                "I love candy. I love cookies. I love cupcakes. \
+                 I love cheesecake. I love chocolate."
+              }
+            />
+            <SnackbarContent
+              message="I love candy. I love cookies. I love cupcakes."
+              action={action}
+            />
+            <SnackbarContent
+              message={
+                "I love candy. I love cookies. I love cupcakes. \
+                 I love cheesecake. I love chocolate."
+              }
+              action={action}
+            />
+          </Stack>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseNotif}>Annuler</Button>
+          {/* <Button onClick={handleSave}>Enregistrer</Button> */}
+        </DialogActions>
+      </Dialog>
+      {openNotif && (
+        <div
+          style={{
+            // position: "left",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            background: "rgba(0, 0, 0, 0.5)",
+            zIndex: 1000,
+            marginLeft: "100%",
+          }}
+        ></div>
+      )}
     </div>
   );
 }
